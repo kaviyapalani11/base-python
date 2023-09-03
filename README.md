b14cb3405e3d8ccfd4748967ffa68bfd85fc4806
kaviya <kaviyapln@gmail.com>
2023-09-03 16:39:47 +0530

    Add files which has data types and arithmetic operations

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # base-python
 Base concepts of python language
+Class 1 : Data types
---
+++ class1.py
@@ -0,0 +1,29 @@
+'''a = 45
+b =65
+c = a+b
+d = c/11 #c/=11
+e = d%5  #d%=5
+print(e)''' 
+'''i =10
+r = type(i)
+print(r)
+salary = 500000.00
+liable_tax_percentage = 10/100
+tax_paid = salary*liable_tax_percentage
+r = type(tax_paid)
+print(r)
+str = "hello"
+r = type(str)
+print(r)
+a = "i'm sick today"
+r = type(a)'''
+input = "abcdefghij"
+'''print (input[0])
+print(input[9])
+name = "jack"
+print(name[0])'''
+#sack
+print('s'+input[::-1])
+print(input[:9])
+print(input[0:9:3])
+
